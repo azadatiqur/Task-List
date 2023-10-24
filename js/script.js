216bb4f68e55280c4c3c90fbc8cffbfe10883887
atiqazad <u2101075@student.cuet.ac.bd>
2023-10-25 05:27:15 +0600

    Define UI Elements
    Selected and stored the UI elements as variables using querySelector.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//Define UI element
+let form = document.querySelector('#task_form');
+let taskList = document.querySelector('ul');
+let clearBtn = document.querySelector('#clear_tasks_btn');
+let filter = document.querySelector("#task_filter");
+let taskInput = document.querySelector('#new_task');
