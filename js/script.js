12b3ecc13dfce86fade8d81b289410c5bc275995
atiqazad <u2101075@student.cuet.ac.bd>
2023-10-25 05:54:34 +0600

    Add the feature of adding task

--- js/script.js
+++ js/script.js
@@ -4,3 +4,30 @@ let taskList = document.querySelector('ul');
 let clearBtn = document.querySelector('#clear_tasks_btn');
 let filter = document.querySelector("#task_filter");
 let taskInput = document.querySelector('#new_task');
+
+
+//Define event listeners
+form.addEventListener('submit', addTask);
+
+
+
+//Define functions
+//Add Task
+function addTask() {
+    if(taskInput.value === '') {
+        alert("Add a task!");
+    }
+    else {
+        //Create li element
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(taskInput.value +
+        " "));//space before cross sign
+        let link = document.createElement('a');
+        link.setAttribute('href', '#');//click option will appear
+        link.innerHTML = 'x';
+        li.appendChild(link);
+        taskList.appendChild(li);
+        taskInput.value = "";//emptying the taskInput value
+    }
+    //e.preventDefault();//prevents the reload after form submit
+}
